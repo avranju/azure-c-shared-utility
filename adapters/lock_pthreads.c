c621d4cfa4738e7c4ebe37f6466561bc6fc3f77d
Rajasekharan Vengalil <rajave@microsoft.com>
2016-06-28 22:01:06 -0700

    Make pthread mutex implementation recursive

--- adapters/lock_pthreads.c
+++ adapters/lock_pthreads.c
@@ -11,98 +11,122 @@ DEFINE_ENUM_STRINGS(LOCK_RESULT, LOCK_RESULT_VALUES);
 /*SRS_LOCK_99_002:[ This API on success will return a valid lock handle which should be a non NULL value]*/
 LOCK_HANDLE Lock_Init(void)
 {
-	pthread_mutex_t* lock_mtx = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
-	if (NULL != lock_mtx)
-	{
-		if (pthread_mutex_init(lock_mtx, NULL) != 0)
-		{
-			/*SRS_LOCK_99_003:[ On Error Should return NULL]*/
-			free(lock_mtx);
-			lock_mtx = NULL;
-			LogError("Failed to initialize mutex");
-		}
-	}
-	
-	return (LOCK_HANDLE)lock_mtx;
+    pthread_mutex_t* lock_mtx;
+    pthread_mutexattr_t attr;
+    if (pthread_mutexattr_init(&attr) != 0)
+    {
+        LogError("pthread_mutexattr_init failed");
+        lock_mtx = NULL;
+    }
+    else
+    {
+        if(pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE) != 0)
+        {
+            LogError("pthread_mutexattr_settype failed");
+            lock_mtx = NULL;
+        }
+        else
+        {
+            lock_mtx = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+            if (NULL != lock_mtx)
+            {
+                if (pthread_mutex_init(lock_mtx, &attr) != 0)
+                {
+                    /*SRS_LOCK_99_003:[ On Error Should return NULL]*/
+                    free(lock_mtx);
+                    lock_mtx = NULL;
+                    LogError("Failed to initialize mutex");
+                }
+            }
+            else
+            {
+                LogError("malloc failed");
+            }
+        }
+
+        pthread_mutexattr_destroy(&attr);
+    }
+    
+    return (LOCK_HANDLE)lock_mtx;
 }
 
 
 LOCK_RESULT Lock(LOCK_HANDLE handle)
 {
-	LOCK_RESULT result;
-	if (handle == NULL)
-	{
-		/*SRS_LOCK_99_007:[ This API on NULL handle passed returns LOCK_ERROR]*/
-		result = LOCK_ERROR;
-		LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
-	}
-	else
-	{
-		if ( pthread_mutex_lock((pthread_mutex_t*)handle) == 0)
-		{
-			/*SRS_LOCK_99_005:[ This API on success should return LOCK_OK]*/
-			result = LOCK_OK;
-		}
-		else
-		{
-			/*SRS_LOCK_99_006:[ This API on error should return LOCK_ERROR]*/
-			result = LOCK_ERROR;
-			LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
-		}
-	}
-	return result;
+    LOCK_RESULT result;
+    if (handle == NULL)
+    {
+        /*SRS_LOCK_99_007:[ This API on NULL handle passed returns LOCK_ERROR]*/
+        result = LOCK_ERROR;
+        LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
+    }
+    else
+    {
+        if ( pthread_mutex_lock((pthread_mutex_t*)handle) == 0)
+        {
+            /*SRS_LOCK_99_005:[ This API on success should return LOCK_OK]*/
+            result = LOCK_OK;
+        }
+        else
+        {
+            /*SRS_LOCK_99_006:[ This API on error should return LOCK_ERROR]*/
+            result = LOCK_ERROR;
+            LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
+        }
+    }
+    return result;
 }
 LOCK_RESULT Unlock(LOCK_HANDLE handle)
 {
-	LOCK_RESULT result;
-	if (handle == NULL)
-	{
-		/*SRS_LOCK_99_011:[ This API on NULL handle passed returns LOCK_ERROR]*/
-		result = LOCK_ERROR;
-		LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
-	}
-	else
-	{
-		if (pthread_mutex_unlock((pthread_mutex_t*)handle) == 0)
-		{
-			/*SRS_LOCK_99_009:[ This API on success should return LOCK_OK]*/
-			result = LOCK_OK;
-		}
-		else
-		{
-			/*SRS_LOCK_99_010:[ This API on error should return LOCK_ERROR]*/
-			result = LOCK_ERROR;
-			LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
-		}
-	}
-	return result;
+    LOCK_RESULT result;
+    if (handle == NULL)
+    {
+        /*SRS_LOCK_99_011:[ This API on NULL handle passed returns LOCK_ERROR]*/
+        result = LOCK_ERROR;
+        LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
+    }
+    else
+    {
+        if (pthread_mutex_unlock((pthread_mutex_t*)handle) == 0)
+        {
+            /*SRS_LOCK_99_009:[ This API on success should return LOCK_OK]*/
+            result = LOCK_OK;
+        }
+        else
+        {
+            /*SRS_LOCK_99_010:[ This API on error should return LOCK_ERROR]*/
+            result = LOCK_ERROR;
+            LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
+        }
+    }
+    return result;
 }
 
 LOCK_RESULT Lock_Deinit(LOCK_HANDLE handle)
 {
-	LOCK_RESULT result=LOCK_OK ;
-	if (NULL == handle)
-	{
-		/*SRS_LOCK_99_013:[ This API on NULL handle passed returns LOCK_ERROR]*/
-		result = LOCK_ERROR;
-		LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
-	}
-	else
-	{
-		/*SRS_LOCK_99_012:[ This API frees the memory pointed by handle]*/
-		if(pthread_mutex_destroy((pthread_mutex_t*)handle)==0)
-		{
-			free(handle);
-			handle = NULL;
-		}
-		else
-		{
-			result = LOCK_ERROR;
-			LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
-		}
-	}
-	
-	return result;
+    LOCK_RESULT result=LOCK_OK ;
+    if (NULL == handle)
+    {
+        /*SRS_LOCK_99_013:[ This API on NULL handle passed returns LOCK_ERROR]*/
+        result = LOCK_ERROR;
+        LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
+    }
+    else
+    {
+        /*SRS_LOCK_99_012:[ This API frees the memory pointed by handle]*/
+        if(pthread_mutex_destroy((pthread_mutex_t*)handle)==0)
+        {
+            free(handle);
+            handle = NULL;
+        }
+        else
+        {
+            result = LOCK_ERROR;
+            LogError("(result = %s)", ENUM_TO_STRING(LOCK_RESULT, result));
+        }
+    }
+    
+    return result;
 }
 
 
